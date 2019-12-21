7a205021b35dccbec708c8c43b4879a7cd7923bd
Ahmad Samawi <asamawi@n4s-solutions.com>
2019-12-21 22:47:24 +0300

    added inital model

--- calc/models.py
+++ calc/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+class Calculation (models.Model):
+    """Calculation for bequest class"""
+    deceased = models.CharField(max_length = 200)
+    amount = models.IntegerField(default=1000)
+
+    def __str__(self):
+        return self.deceased
+
+class Heir(models.Model):
+    """Heir class"""
+
+    heir_name = models.CharField(max_length = 200)
+    calculation = models.ForeignKey(Calculation, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.heir_name
