c59fb9f2da5c3c4bf25345af63c2e0b88fb8f46a
Ahmad Samawi <asamawi@n4s-solutions.com>
2019-12-23 16:01:03 +0300

    added Person and Marriage to Model

--- calc/models.py
+++ calc/models.py
@@ -1,17 +1,36 @@
 from django.db import models
 
+class Person(models.Model):
+    """Person Class"""
+    GENDER_CHOICES = (
+        ('M', 'Male'),
+        ('F', 'Female'),
+    )
+    sex = models.CharField(max_length=1, choices=GENDER_CHOICES)
+    first_name = models.CharField()
+    last_name = models.CharField()
+    parents = models.ForeignKey(Marriage, on_delete=models.SET_NULL)
+
+class Marriage (models.Model):
+    husband = models.ForeignKey(Person, on_delete=models.SET_NULL)
+    wife = models.ForeignKey(Person, on_delete=models.SET_NULL)
+
+
+
+
+
 class Calculation (models.Model):
     """Calculation for bequest class"""
-    deceased = models.CharField(max_length = 200)
+    deceased = Person()
+    name = models.CharField()
     amount = models.IntegerField(default=1000)
 
     def __str__(self):
         return self.deceased
 
-class Heir(models.Model):
+class Heir(Person):
     """Heir class"""
 
-    heir_name = models.CharField(max_length = 200)
     calculation = models.ForeignKey(Calculation, on_delete=models.CASCADE)
 
     def __str__(self):
