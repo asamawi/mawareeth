c1f1891a18426b99f3724bc1ec87b656d6388986
Ahmad Samawi <asamawi@n4s-solutions.com>
2019-12-24 21:53:38 +0300

    add_father() and add_mother() functions added to Person Class

--- calc/models.py
+++ calc/models.py
@@ -11,6 +11,46 @@ class Person(models.Model):
     last_name = models.CharField(max_length=200)
     parents = models.ForeignKey('Marriage',null=True, on_delete=models.SET_NULL)
 
+    def add_father(self, first_name = None, last_name = None):
+        if self.parents and self.parents.husband:
+            return "father already exist"
+        else:
+            if first_name is None:
+                first_name = self.first_name+" Father"
+            if last_name is None:
+                last_name = self.last_name
+            father = Person(sex = 'M', first_name=first_name ,last_name=last_name)
+            father.save()
+
+            #check for parents
+            if self.parents is None:
+                self.parents = Marriage(husband=father)
+            else:
+                self.parents.husband=father
+
+            self.parents.save()
+            return father
+
+    def add_mother(self, first_name = None, last_name = None):
+        if self.parents and self.parents.wife:
+            return "Mother already exist"
+        else:
+            if first_name is None:
+                first_name = self.first_name+" Mother"
+            if last_name is None:
+                last_name = self.last_name
+            mother = Person(sex = 'F', first_name=first_name ,last_name=last_name)
+            mother.save()
+
+            #check for parents
+            if self.parents is None:
+                self.parents = Marriage(wife=mother)
+            else:
+                self.parents.wife = mother
+
+            self.parents.save()
+            return mother
+
     def __str__(self):
         return f"{self.first_name} id: {self.id}"
 
