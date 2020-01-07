bcefdf598157877f216fa0715537d51f593eadad
Ahmad Samawi <asamawi@n4s-solutions.com>
2020-01-08 00:24:03 +0300

    added Travis CI

--- mawareeth/settings.py
+++ mawareeth/settings.py
@@ -92,7 +92,7 @@ WSGI_APPLICATION = 'mawareeth.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'djongo',
+        'ENGINE': os.environ['DB'], #djongo
         'NAME': os.environ['DATABASE_NAME'],
         'HOST':   os.environ['MONGOLAB_URI']  }
 }
