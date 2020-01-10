e62341e21150763e39c1d0195005aa8484fb06e3
Ahmad Samawi <asamawi@n4s-solutions.com>
2020-01-11 01:43:31 +0300

    added test_index

--- calc/tests.py
+++ calc/tests.py
@@ -1,5 +1,13 @@
-from django.test import TestCase
-from calc.models import Person
+from django.test import TestCase, Client
+from calc.models import Person, Calculation
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate
+from django.http.request import HttpRequest
+from django.urls import reverse
+
+from . import views
+
+
 
 class PersonTestCase(TestCase):
     def setUp(self):
@@ -10,3 +18,18 @@ class PersonTestCase(TestCase):
         son = Person.objects.get(first_name="Son")
         self.assertIs(son.add_father(father), father)
         self.assertIs(son.parents.male, father)
+
+class CalculationTestCase(TestCase):
+    def setUp(self):
+        user1 = User.objects.create_user('john', 'lennon@thebeatles.com', 'johnpassword')
+        user2 = User.objects.create_user('sandra', 'bullock@missconj.com', 'sandrapassword')
+        calc1 = Calculation.objects.create(name='calc1', user=user1)
+        calc2 = Calculation.objects.create(name='calc2', user=user2)
+
+    def test_index(self):
+        c = Client()
+        logged_in = c.login(username='john', password='johnpassword')
+        response = c.get('/en/')
+        self.assertTrue(logged_in)
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.context["calculation_list"].count(), 1)
