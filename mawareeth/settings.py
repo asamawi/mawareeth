b6257ea9c7e1af4632d4ea9d25268b6fba724a3b
Ahmad Samawi <asamawi@n4s-solutions.com>
2020-03-31 22:41:50 +0300

    added translation

--- mawareeth/settings.py
+++ mawareeth/settings.py
@@ -128,7 +128,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.0/topics/i18n/
 
-LANGUAGE_CODE = 'ar'
+LANGUAGE_CODE = config("LANGUAGE_CODE",default='ar')
 
 TIME_ZONE = 'Asia/Riyadh'
 
