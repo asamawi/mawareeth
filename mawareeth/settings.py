e45f4cafee82568e194350cb2998c268c0839cb1
Ahmad Samawi <asamawi@n4s-solutions.com>
2020-03-31 07:09:32 +0300

    changed default language to AR

--- mawareeth/settings.py
+++ mawareeth/settings.py
@@ -125,7 +125,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ar'
 
 TIME_ZONE = 'Asia/Riyadh'
 
